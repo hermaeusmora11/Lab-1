0612759534045a8e4f09fd24f0132000418cbf0b
hermaeusmora11 <moribunraou001@gmail.com>
2026-04-09 15:36:57 +0600

    feat: quadratic equation solver

---
+++ src/quadratic.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include "quadratic.h"
+
+int solve_quadratic(double a, double b, double c, double roots[2]) {
+	if (a == 0.0)
+		retirm -1;
+
+	double d = b * b - 4 * a * c;
+
+	if (d < 0)
+		return 0;
+
+	if (d == 0) {
+		roots[0] = -b / (2 * a);
+		return 1;
+	}
+
+	double sqrt_d = sqrt(d);
+
+	roots[0] = (-b - sqrt_d) / (2 * a);
+	roots[1] = (-b + sqrt_d) / (2 * a);
+
+	return 2;
+}
